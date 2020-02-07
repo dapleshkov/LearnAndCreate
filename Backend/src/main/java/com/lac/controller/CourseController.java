afc780ade81b956eff98ad66cea6109df46a99b1
akhpkn <akh.andr@mail.ru>
2020-02-08 01:56:20 +0300

    controllers

---
+++ Backend/src/main/java/com/lac/controller/CourseController.java
@@ -0,0 +1,146 @@
+package com.lac.controller;
+
+import com.lac.model.Course;
+import com.lac.model.Image;
+import com.lac.model.User;
+import com.lac.model.Video;
+import com.lac.payload.UploadFileResponse;
+import com.lac.payload.CourseRequest;
+import com.lac.repository.CourseRepository;
+import com.lac.repository.UserRepository;
+import com.lac.security.CurrentUser;
+import com.lac.security.UserPrincipal;
+import com.lac.service.CourseService;
+import com.lac.service.ImageService;
+import com.lac.service.VideoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    private VideoService videoService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        List<Course> courses = courseRepository.findAll();
+        return new ResponseEntity<>(courses, HttpStatus.OK);
+    }
+
+    @PostMapping("/{courseId}/image")
+    public UploadFileResponse setCourseImage(@RequestParam(name = "file") MultipartFile file,
+                                             @PathVariable("courseId") Long courseId) throws IOException {
+        Course course = courseRepository.findByCourseId(courseId);
+
+        Image image = imageService.store(file);
+        course.setImage(image);
+        courseRepository.save(course);
+
+        return new UploadFileResponse(image.getName(), image.getType(), file.getSize());
+    }
+
+    @PostMapping("/{courseId}/videos")
+    public List<UploadFileResponse> setCourseVideos(@RequestParam(name = "files") MultipartFile[] files,
+                                                    @PathVariable("courseId") Long courseId) throws IOException {
+        Course course = courseRepository.findByCourseId(courseId);
+
+        Set<Video> videos = new HashSet<>();
+        List<UploadFileResponse> responses = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            Video video = videoService.store(file);
+            videos.add(video);
+            responses.add(new UploadFileResponse(video.getName(), video.getType(), file.getSize()));
+        }
+
+        course.setVideos(videos);
+        courseRepository.save(course);
+
+        return responses;
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getCoursesByUserId(@PathVariable("userId") Long userId) {
+        User user = userRepository.findByUserId(userId);
+        if (user != null) {
+            Set<Course> courses = user.getCourses();
+            return new ResponseEntity<>(courses, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getMyCourses(@CurrentUser UserPrincipal currentUser) {
+        User user = userRepository.findByUserId(currentUser.getUserId());
+        if (user != null) {
+            Set<Course> courses = user.getCourses();
+            return new ResponseEntity<>(courses, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> addCourse(@Valid @RequestBody CourseRequest request) {
+        Course course = new Course(request.getTitle(), request.getDescription());
+        boolean flag = courseService.addCourse(course);
+        if (!flag)
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/api/courses/{title}")
+                .buildAndExpand(course.getCourseId()).toUri());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{courseId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Void> subscribeCourse(@CurrentUser UserPrincipal currentUser,
+                                                @PathVariable("courseId") Long courseId) {
+        boolean flag = courseService.subscribeCourse(currentUser, courseId);
+        if (flag)
+            return new ResponseEntity<>(HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    @DeleteMapping("/{courseId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Void> unsubscribeCourse(@CurrentUser UserPrincipal currentUser,
+                                                  @PathVariable("courseId") Long courseId) {
+        boolean flag = courseService.unsubscribeCourse(currentUser, courseId);
+        if (flag)
+            return new ResponseEntity<>(HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+}
---
+++ Backend/src/main/java/com/lac/controller/UserController.java
@@ -0,0 +1,104 @@
+package com.lac.controller;
+
+import com.lac.model.Image;
+import com.lac.model.User;
+import com.lac.payload.UploadFileResponse;
+import com.lac.repository.UserRepository;
+import com.lac.security.CurrentUser;
+import com.lac.security.UserPrincipal;
+import com.lac.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<User> getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        User user = userRepository.findByUserId(currentUser.getUserId());
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/user/me/edit/name")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> changeName(@CurrentUser UserPrincipal currentUser,
+                                         @RequestParam(name = "name") String name) {
+        if (name.length() > 20)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        User user = userRepository.findByUserId(currentUser.getUserId());
+        user.setName(name);
+        userRepository.save(user);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    @PostMapping("/user/me/edit/username")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> changeUsername(@CurrentUser UserPrincipal currentUser,
+                                            @RequestParam(name = "username") String username) {
+        if (username.length() > 15 || username.length() < 4)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (userRepository.existsByUsernameOrEmail(username, username))
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        User user = userRepository.findByUserId(currentUser.getUserId());
+        user.setUsername(username);
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("user/me/edit/password")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> changePassword(@CurrentUser UserPrincipal currentUser,
+                                            @RequestParam(name = "password") String password) {
+        if (password.length() < 4 || password.length() > 20)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        User user = userRepository.findByUserId(currentUser.getUserId());
+        user.setPassword(passwordEncoder.encode(password));
+        userRepository.save(user);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @PostMapping("/user/me/image")
+    @PreAuthorize("hasRole('USER')")
+    public UploadFileResponse setUserImage(@CurrentUser UserPrincipal currentUser,
+                                           @RequestParam("file") MultipartFile file) throws IOException {
+        User user = userRepository.findByUserId(currentUser.getUserId());
+        Image image = imageService.store(file);
+
+        user.setImage(image);
+
+        userRepository.save(user);
+
+        return new UploadFileResponse(image.getName(), image.getType(), file.getSize());
+    }
+
+    @GetMapping("user/me/imagetype")
+    public MediaType getImageContentType(@CurrentUser UserPrincipal currentUser) {
+        User user = userRepository.findByUserId(currentUser.getUserId());
+        Image image = user.getImage();
+        MediaType mediaType = MediaType.valueOf(image.getType());
+        return mediaType;
+    }
+}
